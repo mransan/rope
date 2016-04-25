5cc20a361e1c2cfe9ccf1783ea56626a4157cf7f
mransan <maxime.ransan@gmail.com>
2016-04-24 21:50:33 -0400

    Add comment and fix empty rope iterator bug

--- rope.h
+++ rope.h
@@ -11,7 +11,6 @@ class t;
 class iterator;
 class const_iterator;
 
-
 // @brief Rope Class which implements basic functionality 
 // of the Rope data structure for std::string. 
 //
@@ -19,48 +18,90 @@ class const_iterator;
 class t {
 
 public:
+
   // Creators
   // --------
 
+  // @brief Initialize the rope by moving a string.
+  //
+  // Most recommended constructor since no memory allocation
+  // will be performed.
   explicit t(std::string&& s);
 
+  // @brief Initialize the rope by copying a string. 
+  //
+  // A memory allocation + copy is done for the length 
+  // of the string [s].
+  explicit t(std::string const& s);
+
+  // @bief Initialize the rope to be an append node 
+  // of 2 sub ropes.
   t(t&& lhs, 
     t&& rhs);
 
+  // @brief Move Constructor
   t(t&& copy);
 
   // Destructors 
   // -----------
 
+  // @brief Destructor
   ~t();
 
   // Accessors
   // ---------
 
+  // @brief total size of the rope string 
+  //
+  // It's the sum of all the sub strings (ie leaves). 
   std::size_t size() const;
 
+  // @brief return the height of the rope. 
+  // - Internal height of the rope tree data structure
+  // - Only used for testing
   std::size_t height() const;
-    // Internal height of the rope tree data structure
-    // Only used for testing
 
+  // @brief non const character accessor.
   char& operator[](std::size_t index);
+  
+  // @brief const character accessor.
   char  operator[](std::size_t index) const;
 
   // Modifiers
   // ---------
   
+  // @brief move and append a string to the rope. 
   t& append_string(std::string&& rhs); 
+  
+  // @brief append a string to the rope. 
+  //
+  // allocation+copy will be made.
+  t& append_string(std::string const& rhs); 
 
   // Iterators
   // ---------
   
+  // @brief iterator to the begining of the rope. 
   iterator begin(); 
 
+  // @brief iterator to the end of the rope. 
+  //
+  // dereferencing this iterator value is undefined
+  // behavior.
   iterator end(); 
 
+  // @brief const iterator to the begining of the rope. 
   const_iterator begin() const;
 
+  // @brief const iterator to the end of the rope. 
+  //
+  // dereferencing this iterator value is undefined
+  // behavior.
   const_iterator end() const;
+
+private:
+
+  friend class iterator;
 
 private:
 
@@ -74,7 +115,8 @@ private:
            t&& rhs, 
            t*  self);
 
-    append(append&& copy, t* self);
+    append(append&& copy, 
+           t* self);
 
     std::unique_ptr<t> _lhs; 
     std::unique_ptr<t> _rhs;
@@ -86,18 +128,23 @@ private:
   };
 
 private:
-  friend class iterator;
+
+  // @brief return the left most string of the rope data structure. 
   t* left_most_string();
+
+  // @brief return the next string when traversing the rope in a depth
+  // first left to right manner. 
+  //
+  // @return nullptr if no more leaf string 
+  // nodes
   t* next_string_leaf(); 
-    // return nullptr if no more leaf string 
-    // nodes
   
 private:
 
-  tag         _tag;
-  std::size_t _size;
-  std::size_t _height;
-  t*          _parent; // nullptr if Root node.
+  tag         tag_;
+  std::size_t size_;
+  std::size_t height_;
+  t*          parent_; // nullptr if Root node.
 };
 
 class iterator 
